31d702e3e3992fe4b4fbef394802a85282e2c404
Tommy Tsui <tommy.tsui@yedpay.com.hk>
2024-06-30 08:42:05 -0700

    feat: Update JWT module configuration to use async factory

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '5h' },
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '5h' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
